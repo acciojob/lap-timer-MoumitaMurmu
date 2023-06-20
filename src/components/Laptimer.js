f3e7684bf611d2f0ab7836a912e044055b8af4ac
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-06-20 08:28:10 +0530

    Create Laptimer.js

---
+++ src/components/Laptimer.js
@@ -0,0 +1,59 @@
+import React,{useState,useEffect} from "react";
+
+const Laptimer = () => {
+    const[time,setTime] = useState(0)
+    const[lap,setLap] = useState([])
+    const[isRunning,setIsRunning] = useState(false)
+
+    useEffect(()=>{
+        let intervalid ;
+if(isRunning)
+{
+        intervalid = setInterval(()=>{setTime((prevtime)=>prevtime+10)},10)
+    }
+    return () => clearInterval(intervalid)
+    },[isRunning])
+    
+    const handleStart = () => {
+        setIsRunning(true);
+      };
+    
+    const handleStop = () => {
+      setIsRunning(false)
+    }
+      const handleReset = () => {
+        setIsRunning(false)
+        setTime(0);
+        setLap([]);
+      };
+    
+      const handleLap = () => {
+        setLap((prevLaps) => [...prevLaps, time]);
+      };
+    const minutes = Math.floor(time / 60000);
+  const seconds = Math.floor((time / 1000) % 60);
+  const milliseconds = Math.floor(time % 1000);
+    return(
+    <>
+    <div>
+      {minutes.toString().padStart(2, "0")}:
+      {seconds.toString().padStart(2, "0")}:
+      {milliseconds.toString().padStart(2, "0").slice(0, 2)}
+    </div>
+    <button onClick={handleStart}> Start </button>
+    <button onClick={handleStop}> Stop </button>
+    <button onClick={handleReset}> Reset </button>
+    <button onClick={handleLap}> Lap </button>
+<div>
+{
+lap.map((t)=>(<li>
+{Math.floor(t/60000).toString().padStart(2, "0")}:
+{Math.floor((t/1000)%60).toString().padStart(2, "0")}:
+{ Math.floor(t % 1000).toString().slice(0, 2)}</li>))
+}
+</div>
+  </>
+);
+}
+
+export default Laptimer ;
