d71e43dd590ad1622fc849aa2c44901411074fdb
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-06-20 08:27:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Laptimer from "./Laptimer";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Laptimer/>
     </div>
   )
 }
